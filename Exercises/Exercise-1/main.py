6f7320e27c1ee11366033cf2abf16748632a3c79
Antoine BRUN <abrun@e3r9p6.clusters.42paris.fr>
2022-12-10 18:22:00 +0100

    exo 2 todo

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,5 +1,12 @@
 import requests
 import os
+from os.path import exists
+from zipfile import ZipFile
+import aiohttp
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+import time
+import random
 
 download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip',
@@ -20,14 +27,78 @@ def	createDir(name_dir):
 	if not os.path.exists(path):
 		os.mkdir(path);
 
-def	downloadUris(uris):
-	for uri in uris:
-		response = requests.get(uri)
-		if response.status_code == 200:
+def	downloadUri(uri):
+	response = requests.get(uri)
+	if response.status_code == 200:
+		spliter = uri.split('/');
+		filename = spliter[len(spliter) - 1];
+		file_location = "./" + download_dir + "/"; 
+		print("Downloading " + filename);
+		if not exists(file_location + filename):
+			if open(file_location + filename, "wb").write(response.content) == False:
+				print("Cannot write content in " + file_location + filename);
+			else:
+				print(filename + " downloaded !")
+				spliter = filename.split('.');
+				if spliter[len(spliter) - 1] == "zip":
+					lenLoop = len(spliter) - 2;
+					i = 0;
+					unzipFile = "";
+					while i <= lenLoop:
+						unzipFile += spliter[i];
+						i += 1;
+					print("Extracting " + unzipFile);
+					with ZipFile(file_location + filename, 'r') as zObject:
+						zObject.extractall(path=file_location);
+					zObject.close();
+					os.remove(file_location + filename);
+					print("Zip file removed");
+		else:
+			print("File already exist");
+	else:
+		print('URL passed is invalid');
+
+async def	downloadUriAsync(dirname, uri):
+	async with aiohttp.ClientSession() as session:
+		async with session.get(uri) as response:
+			if response.status == 200:
+				spliter = uri.split('/');
+				filename = spliter[len(spliter) - 1];
+				file_location = "./" + dirname + "/"; 
+				print("Downloading " + filename);
+				content = await response.read();
+				if not exists(file_location + filename):
+					if open(file_location + filename, "wb").write(content) == False:
+						print("Cannot write content in " + file_location + filename);
+					else:
+						print(filename + " downloaded !")
+						spliter = filename.split('.');
+						if spliter[len(spliter) - 1] == "zip":
+							lenLoop = len(spliter) - 2;
+							i = 0;
+							unzipFile = "";
+							while i <= lenLoop:
+								unzipFile += spliter[i];
+								i += 1;
+							print("Extracting " + unzipFile);
+							with ZipFile(file_location + filename, 'r') as zObject:
+								zObject.extractall(path=file_location);
+							zObject.close();
+							os.remove(file_location + filename);
+							print("Zip file removed");
+				else:
+					print("File already exist");
+			else:
+				print('URL passed is invalid');
+
+def	downloadUrisMultiThreads(dirname, uris):
+	with ThreadPoolExecutor(max_workers=len(uris)) as executor:
+		executor.map(downloadUri, uris);
+				
 
 def main():
-	createDir(download_dir)
-	downloadUris(download_uris)
+	createDir(download_dir);
+	downloadUrisMultiThreads(download_dir, download_uris);
 	pass
 
 
--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,11 +1,24 @@
 import requests
 import pandas
+from bs4 import BeautifulSoup
 
+weether_url = "https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/";
 
+def	getPageContent(url):
+	response = requests.get(url);
+	if (response.status_code == 200):
+		return (response.content);
+
+def	displayHtmlContent(html_content):
+	soup = BeautifulSoup(html_content, 'html.parser');
+	print(soup.prettify());
 
 def main():
-    # your code here
-    pass
+	page_content = getPageContent(weether_url);
+	if (not page_content):
+		return ;
+	displayHtmlContent(page_content)
+	pass
 
 
 if __name__ == '__main__':
